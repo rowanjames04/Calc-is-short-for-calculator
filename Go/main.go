8bbe4a24c32c9e7635db338bbc82332ac4bb5dfc
rowanjames04 <r04james@outlook.com>
2025-12-25 19:31:40 +1100

    build: Go calc basic functionality completed

--- Go/main.go
+++ Go/main.go
@@ -6,21 +6,62 @@ import (
 
 func main() {
 	running := true
+	validari := true
 
 	var quit string
+	var ari string
+	var arisym string
+
+	var num1 int
+	var num2 int
+	var ans int
 
 	fmt.Println("Hello welcome to calc")
 	for running {
-		fmt.Print("Do you want to quit: ")
+		fmt.Print("\nDo you want to quit: ")
 		fmt.Scan(&quit)
 		quit = strings.ToLower(strings.TrimSpace(quit))
 		if quit == "yes" || quit == "y" {
-			fmt.Println("/n Exiting...")
+			fmt.Println("\nExiting...")
 			running = false
 		} else if quit == "no" || quit == "n" {
 
+			fmt.Print("\nEnter your first number: ")
+			fmt.Scan(&num1)
+
+			fmt.Print("\nEnter your second number: ")
+			fmt.Scan(&num2)
+			
+			validari = true
+
+			for validari {
+				fmt.Print("\nWould you like to add, subtract, divide or multiply these numbers: ")
+				fmt.Scan(&ari)
+				
+				switch ari {
+					case "add":
+						ans = add(num1, num2)
+						validari = false
+						arisym = "+"
+					case "subtract":
+						ans = subtract(num1, num2)
+						validari = false
+						arisym = "-"
+					case "multiply":
+						ans = multiply(num1, num2)
+						validari = false
+						arisym = "x"
+					case "divide":
+						ans = divide(num1, num2)
+						validari = false
+						arisym = "/"
+					default:
+						fmt.Println("Try Again")
+				}
+			}
+
+			fmt.Printf("%v %v %v = %v\n", num1, arisym, num2, ans)
 		}
-
 	}
-}
 
+}
---
+++ Go/math.go
@@ -0,0 +1,18 @@
+package main
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func subtract(a int, b int) int {
+	return a - b
+}
+
+func multiply(a int, b int) int {
+	return a * b
+}
+
+func divide(a int, b int) int {
+	return a / b
+}
+
